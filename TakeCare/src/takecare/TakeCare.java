05187df0a64f27fc5d9a1a1c9b1d7d6ca1dee2a9
Mr.wemm@gmail.com <Mr.wemm@gmail.com>
2018-03-06 16:58:11 +0100

    Start committing

---
+++ TakeCare/src/takecare/TakeCare.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package takecare;
+
+/**
+ *
+ * @author User
+ */
+public class TakeCare {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Take Care");
+    }
+    
+}
